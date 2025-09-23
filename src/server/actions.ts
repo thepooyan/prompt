96fae72e44624c9f6a5496e18bdb7808f2596c29
pooyan <thepooyan@gmail.com>
2025-09-23 16:01:13 +0330

    action for uploading files

---
+++ src/server/actions.ts
@@ -0,0 +1,21 @@
+"use server"
+
+import { s3 } from "@/s3"
+import { PutObjectCommand } from "@aws-sdk/client-s3"
+
+export async function uploadToS3(file: File) {
+  const arrayBuffer = await file.arrayBuffer()
+  const buffer = Buffer.from(arrayBuffer)
+
+  const key = `${Date.now()}-${file.name}`
+
+  await s3.send(new PutObjectCommand({
+    Bucket: "pooyan",
+    Key: key,
+    Body: buffer,
+    ContentType: file.type,
+     ACL: "public-read"
+  }))
+
+  return `https://${process.env.BUCKET_URL}/${key}`
+}
