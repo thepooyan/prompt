2ff102663b608c42fcfa89e3ef9c67c269079a12
pooyan <thepooyan@gmail.com>
2025-10-01 15:07:14 +0330

    build

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,18 +22,18 @@ export const metadata: Metadata = {
     index: true,
     follow: true,
   },
-  structuredData: {
-    '@context': 'http://schema.org',
-    '@type': 'WebPage',
-    name: 'پرامپت بازار',
-    description: 'پرامپت بازار یک پلتفرم آنلاین است که بهترین پرامپت‌ها برای مدل‌های هوش مصنوعی را ارائه می‌دهد.',
-    url: 'https://promptbazar.ir',
-    publisher: {
-      '@type': 'Organization',
-      name: 'پرامپت بازار',
-      logo: 'https://yourwebsite.com/logo.png',
-    },
-  },
+  // structuredData: {
+  //   '@context': 'http://schema.org',
+  //   '@type': 'WebPage',
+  //   name: 'پرامپت بازار',
+  //   description: 'پرامپت بازار یک پلتفرم آنلاین است که بهترین پرامپت‌ها برای مدل‌های هوش مصنوعی را ارائه می‌دهد.',
+  //   url: 'https://promptbazar.ir',
+  //   publisher: {
+  //     '@type': 'Organization',
+  //     name: 'پرامپت بازار',
+  //     logo: 'https://yourwebsite.com/logo.png',
+  //   },
+  // },
 };
 
 export default function RootLayout({
