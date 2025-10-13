a95bb967e6957bc1326b4a7ab984200737e00379
pooyan <thepooyan@gmail.com>
2025-10-13 15:18:22 +0330

    nothing!

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -17,3 +17,27 @@ export const limitChar = (string: string, limit: number) => {
   if (string.length < limit) return string
   return string.substring(0, limit-3) + "..."
 }
+
+type func = () => void
+export class CallbackStore {
+  private yes: func | null = null
+  private no: func | null = null
+  setYes(callback: ()=>void) {
+    this.yes = callback
+  }
+  setNo(callback: ()=>void) {
+    this.no = callback
+  }
+  callYes() {
+    this.yes && this.yes()
+    this.clear()
+  }
+  callNo() {
+    this.no && this.no()
+    this.clear()
+  }
+  private clear() {
+    this.yes = null
+    this.no = null
+  }
+}
