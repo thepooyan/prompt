6a93a66153a63329133a797d5ad005066bc8247a
pooyan <thepooyan@gmail.com>
2025-10-13 09:24:12 +0330

    nothing!

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,16 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export const readableDate = (date: string) => {
+  return new Date(date).toLocaleDateString("fa", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  })
+}
+
+export const limitChar = (string: string, limit: number) => {
+  if (string.length < limit) return string
+  return string.substring(0, limit-3) + "..."
+}
