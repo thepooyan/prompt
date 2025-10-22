d838adf277254ec8fc4394ccfd5c7e3559104f3e
pooyan <thepooyan@gmail.com>
2025-10-22 11:41:14 +0330

    nothing!

--- src/app/Admin/PromptManagment/page.tsx
+++ src/app/Admin/PromptManagment/page.tsx
@@ -1,6 +1,12 @@
-const page = () => {
+import PromptManagmentClient from "@/components/pages/PromptManagmentClient"
+import { fetchPrompts } from "@/server/dataFetching"
+
+const page = async () => {
+
+  const data = await fetchPrompts()
+
   return (
-    <div>page</div>
+    <PromptManagmentClient initialBlogs={data}/>
   )
 }
 
--- src/server/mutation.ts
+++ src/server/mutation.ts
@@ -1,6 +1,6 @@
 "use server"
 import { db } from "@/db"
-import { blogsTable } from "@/db/schema"
+import { blogsTable, promptsTable } from "@/db/schema"
 import { eq } from "drizzle-orm"
 import { updateTag } from "next/cache"
 import { cacheTags } from "./cache"
@@ -9,6 +9,16 @@ export const deletePost = async (id: number) => {
     try {
         await db.delete(blogsTable).where(eq(blogsTable.id, id))
         updateTag(cacheTags.blogs)
+        return {ok: true}
+    } catch (e) {
+        return {ok: false, error:e}
+    }
+}
+
+export const deletePrompt = async (id: number) => {
+    try {
+        await db.delete(promptsTable).where(eq(promptsTable.id, id))
+        updateTag(cacheTags.prompts)
         return {ok: true}
     } catch (e) {
         return {ok: false, error:e}
