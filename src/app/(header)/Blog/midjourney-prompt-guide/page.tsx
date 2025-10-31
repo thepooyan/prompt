d0a7341fc19552fcec2e1aff7ff92d61f4f57b66
pooyan <thepooyan@gmail.com>
2025-10-31 15:41:46 +0330

    nothing!

--- src/app/(header)/Blog/midjourney-prompt-guide/page.tsx
+++ src/app/(header)/Blog/midjourney-prompt-guide/page.tsx
@@ -1,4 +1,5 @@
 import MidjournyBlog from "@/components/blog/Midjourny"
+import { fetchSingleBlog } from "@/server/dataFetching";
 import { Metadata } from "next"
 
 const page = () => {
@@ -7,13 +8,17 @@ const page = () => {
   )
 }
 
-export const metadata:Metadata = {
-  title: "آموزش پرامپت نویسی میدجورنی",
-  description: "راهنمای کامل و قدم به قدم آموزش پرامپت نویسی میدجرنی برای ساخت عکس‌های حرفه‌ای و هنری. با مثال‌های کاربردی و تکنیک‌های پیشرفته پرامپت Midjourney آشنا شوید.",
-  keywords: "آموزش میدجرنی, پرامپت نویسی میدجرنی, پرامپت میدجرنی, ساخت عکس با هوش مصنوعی, Midjourney, پرامپت بازار, هوش مصنوعی تصویرساز",
-  alternates: {
-    canonical: "https://promptbazar.ir/blog/midjourney-prompt-guide"
+export const generateMetadata = async (): Promise<Metadata> => { 
+  const data = await fetchSingleBlog("midjourney-prompt-guide")
+
+  if (!data) throw new Error("midjourney-prompt-guide not found")
+
+  return {
+    title: data.seoTitle,
+    description: data.seoDescription,
+    keywords: data.seoKeywords,
+    alternates: {canonical: data.canonical}
   }
-}
+};
 
 export default page
