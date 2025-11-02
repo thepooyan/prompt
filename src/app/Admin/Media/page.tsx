c338c97ddcd4b1737922473ea72e4b401a3a678d
pooyan <thepooyan@gmail.com>
2025-11-02 13:27:18 +0330

    nothing!

--- src/app/Admin/Media/page.tsx
+++ src/app/Admin/Media/page.tsx
@@ -1,10 +1,13 @@
+import MediaCard from "@/components/admin/MediaCard"
 import { getAllFiles } from "@/server/s3Actions"
 
 const page = async () => {
     const files = await getAllFiles()
   return (
     <div>
-        {files.map(f => <div key={f}>{f}</div>)}
+      <div className="grid grid-cols-4 gap-y-4 p-5 ">
+        {files.map(f => <MediaCard>{f}</MediaCard>)}
+      </div>
     </div>
   )
 }
---
+++ src/components/admin/MediaCard.tsx
@@ -0,0 +1,36 @@
+"use client"
+import Image from "next/image"
+import { Button } from "../ui/button"
+import { Copy, Trash } from "lucide-react"
+import { callModal } from "../layout/Modal"
+import { copyToClipboard } from "@/lib/utils"
+
+interface p {
+    children: string
+}
+
+const MediaCard = ({children}:p) => {
+    const url = `https://c327107.parspack.net/${children}`
+
+    const copy = () => {
+        copyToClipboard(url)
+    }
+    const remove = () => {
+        callModal.prompt("حذف شود؟")
+        .yes(() => {
+
+        })
+    }
+
+  return (
+    <div className="bg-zinc-900 cursor-pointer transition-colors p-5 w-max rounded flex justify-between items-center flex-col gap-5">
+        <Image width={200} height={200} src={url} alt=""/>
+        <div className="space-x-1">
+            <Button variant="secondary" onClick={copy}><Copy/></Button>
+            <Button variant="secondary" onClick={remove}><Trash className="text-destructive font-bold" /></Button>
+        </div>
+    </div>
+  )
+}
+
+export default MediaCard
