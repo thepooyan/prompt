4e9b0e3218791b7fc16d07ce3a0c8f7f993e2d8d
pooyan <thepooyan@gmail.com>
2025-11-02 14:38:12 +0330

    nothing!

--- src/app/Admin/Media/page.tsx
+++ src/app/Admin/Media/page.tsx
@@ -1,7 +1,6 @@
+import MediaBtnClient from "@/components/admin/MediaBtnClient"
 import MediaCard from "@/components/admin/MediaCard"
-import UploadMediaBtn from "@/components/admin/UploadMediaBtn"
 import { getAllFiles } from "@/server/s3Actions"
-import { Upload } from "lucide-react"
 import { headers } from "next/headers"
 
 const page = async () => {
@@ -9,10 +8,7 @@ const page = async () => {
   const files = await getAllFiles()
   return (
     <div className="p-5">
-      <UploadMediaBtn>
-        آپلود
-        <Upload/>
-      </UploadMediaBtn>
+      <MediaBtnClient/>
 
       <div className="grid grid-cols-4 gap-y-4 ">
         {files.map(f => <MediaCard key={f}>{f}</MediaCard>)}
