69969096efad8770dc8aa851d455a473f61594c5
pooyan <thepooyan@gmail.com>
2025-11-03 02:29:44 +0330

    nothing!

--- src/components/parts/Logo.tsx
+++ src/components/parts/Logo.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 const Logo = () => {
   return (
     <Link href="/" className="flex items-center justify-center gap-3">
-      <Image src="/logo.png" width={100} height={100} alt="prompt Bazar's logo"/>
+      <Image src="/logo.png" width={100} height={100} alt="prompt Bazar's logo" className="w-18 h-18 md:w-auto md:h-auto"/>
 
       <div className="ltr text-center">
         <p className="font-bold text-lg hidden sm:block">
