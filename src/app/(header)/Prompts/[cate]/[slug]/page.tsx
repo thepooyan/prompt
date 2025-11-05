1a0ce988c46c6a723ddc5ec0c908902c72d07922
pooyan <thepooyan@gmail.com>
2025-11-05 20:03:55 +0330

    nothing!

--- src/app/(header)/Prompts/[cate]/[slug]/page.tsx
+++ src/app/(header)/Prompts/[cate]/[slug]/page.tsx
@@ -1,13 +1,39 @@
-import LibraryClient from "@/components/LibraryClient"
-import { fetchPrompts } from "@/server/dataFetching"
+import PromptPageClient from "@/components/pages/PromptPageClient"
+import { fetchSinglePrompt, fetchTwoPrompts } from "@/server/dataFetching"
+import { env } from "@/server/env"
+import { Metadata } from "next"
+import { notFound } from "next/navigation"
 
-const page = async () => {
-
-    let p = await fetchPrompts()
-
+interface p {
+  params: Promise<{slug: string}>
+}
+const page = async ({params}:p) => {
+  const {slug} = await params
+  const [data, related] = await Promise.all([fetchSinglePrompt(slug), fetchTwoPrompts()])
+  if (!data) return notFound()
   return (
-    <LibraryClient prompts={p}/>
+    <>
+      <PromptPageClient prompt={data} relatedPrompts={related}/>
+    </>
   )
+}
+
+ 
+export async function generateMetadata( { params }: p ): Promise<Metadata> {
+  
+  const { slug } = await params
+  const data = await fetchSinglePrompt(slug)
+
+  if (!data) return {}
+ 
+  return {
+    title: data.seoTitle,
+    description: data.seoDescription,
+    keywords: data.seoKeywords,
+    alternates: {
+      canonical: `${env.BETTER_AUTH_URL}/Prompts/${data.slug}`
+    }
+  }
 }
 
 export default page
