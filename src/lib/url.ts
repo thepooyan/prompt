b34de5c59c78573a37e9a405d38f9a80aa71b03f
pooyan <thepooyan@gmail.com>
2025-12-02 16:39:41 +0330

    nothing!

--- src/lib/url.ts
+++ src/lib/url.ts
@@ -1,8 +1,8 @@
-import { getEntityName } from "./utils"
+import { capitalize } from "./utils"
 import { PromptWithRelations } from "@/server/dataFetching"
 
 export const weblogDetailsUrl = (slug: string) => `/Blog/${encodeURIComponent(slug)}`
 
-export const promptDetailsUrl = (pr: PromptWithRelations) => `/${getEntityName(pr.type)}/${pr.category.slug}/${encodeURIComponent(pr.slug)}`
+export const promptDetailsUrl = (pr: PromptWithRelations) => `/${capitalize(pr.type)}/${pr.category.slug}/${encodeURIComponent(pr.slug)}`
 
 export const editPromptUrl = (id: number) => `/Admin/EditPrompt/${id}`
